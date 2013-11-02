5223402b5fe46b9ecfceeabf6e372e7762818082
Wing Lung Ngai <w.l.ngai@student.tudelft.nl>
2013-11-02 17:10:39 +0100

    basic funcationalities like run, configure , retrieve instances and terminate

--- src/main/java/nl/tudelft/ec2interface/EC2Interface.java
+++ src/main/java/nl/tudelft/ec2interface/EC2Interface.java
@@ -1,217 +1,183 @@
-/*
- * Copyright 2010-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
 package nl.tudelft.ec2interface;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.ListObjectsRequest;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.DescribeInstanceStatusRequest;
+import com.amazonaws.services.ec2.model.DescribeInstanceStatusResult;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.InstanceStatus;
+import com.amazonaws.services.ec2.model.Placement;
+import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
+import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 
-/**
- * This sample demonstrates how to make basic requests to Amazon S3 using
- * the AWS SDK for Java.
- * <p>
- * <b>Prerequisites:</b> You must have a valid Amazon Web Services developer
- * account, and be signed up to use Amazon S3. For more information on
- * Amazon S3, see http://aws.amazon.com/s3.
- * <p>
- * <b>Important:</b> Be sure to fill in your AWS access credentials in the
- *                   AwsCredentials.properties file before you try to run this
- *                   sample.
- * http://aws.amazon.com/security-credentials
- */
 public class EC2Interface {
 
-    public static void main(String[] args) throws IOException {
-        /*
-         * This credentials provider implementation loads your AWS credentials
-         * from a properties file at the root of your classpath.
-         *
-         * Important: Be sure to fill in your AWS access credentials in the
-         *            AwsCredentials.properties file before you try to run this
-         *            sample.
-         * http://aws.amazon.com/security-credentials
-         */
-        AmazonS3 s3 = new AmazonS3Client(new ClasspathPropertiesFileCredentialsProvider());
-        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
-        s3.setRegion(usWest2);
+	private AmazonEC2 ec2;
 
-        String bucketName = "my-first-s3-bucket-" + UUID.randomUUID();
-        String key = "MyObjectKey";
+	public static void main(String[] args) throws IOException {
+		EC2Interface ec2 = new EC2Interface();
+		System.out.println(ec2.runNewInstance());
+		//System.out.println(ec2.getInstanceInfo("i-4c0d6316"));
+		//System.out.println(ec2.getInstanceList().toString());
+		//ec2.terminateInstance("i-4c0d6316");
+		//ec2.terminateInstances(new ArrayList<String>(){{add("i-b8e4a1e3");add("i-5f879804");}});
+	}
+	
+	public EC2Interface() {
+		
+		// Create the AmazonEC2Client object so we can call various APIs.
+		ec2 = new AmazonEC2Client(new ClasspathPropertiesFileCredentialsProvider());
+		Region usWest1 = Region.getRegion(Regions.US_WEST_1);
+		ec2.setRegion(usWest1);
+	}
+	
+	public String runNewInstance()
+	{
+		RunInstancesRequest runInstancesRequest = new RunInstancesRequest();
 
-        System.out.println("===========================================");
-        System.out.println("Getting Started with Amazon S3");
-        System.out.println("===========================================\n");
+		runInstancesRequest
+				.withImageId("ami-acf9cde9")
+				.withInstanceType("t1.micro")
+				.withMinCount(1)
+				.withMaxCount(1)
+				.withKeyName("joseph_wing")
+				.withSecurityGroups("sg_testinstance1")
+				.withPlacement(new Placement("us-west-1c"));
 
-        try {
-            /*
-             * Create a new S3 bucket - Amazon S3 bucket names are globally unique,
-             * so once a bucket name has been taken by any user, you can't create
-             * another bucket with that same name.
-             *
-             * You can optionally specify a location for your bucket if you want to
-             * keep your data closer to your applications or users.
-             */
-            System.out.println("Creating bucket " + bucketName + "\n");
-            s3.createBucket(bucketName);
-
-            /*
-             * List the buckets in your account
-             */
-            System.out.println("Listing buckets");
-            for (Bucket bucket : s3.listBuckets()) {
-                System.out.println(" - " + bucket.getName());
-            }
-            System.out.println();
-
-            /*
-             * Upload an object to your bucket - You can easily upload a file to
-             * S3, or upload directly an InputStream if you know the length of
-             * the data in the stream. You can also specify your own metadata
-             * when uploading to S3, which allows you set a variety of options
-             * like content-type and content-encoding, plus additional metadata
-             * specific to your applications.
-             */
-            System.out.println("Uploading a new object to S3 from a file\n");
-            s3.putObject(new PutObjectRequest(bucketName, key, createSampleFile()));
-
-            /*
-             * Download an object - When you download an object, you get all of
-             * the object's metadata and a stream from which to read the contents.
-             * It's important to read the contents of the stream as quickly as
-             * possibly since the data is streamed directly from Amazon S3 and your
-             * network connection will remain open until you read all the data or
-             * close the input stream.
-             *
-             * GetObjectRequest also supports several other options, including
-             * conditional downloading of objects based on modification times,
-             * ETags, and selectively downloading a range of an object.
-             */
-            System.out.println("Downloading an object");
-            S3Object object = s3.getObject(new GetObjectRequest(bucketName, key));
-            System.out.println("Content-Type: "  + object.getObjectMetadata().getContentType());
-            displayTextInputStream(object.getObjectContent());
-
-            /*
-             * List objects in your bucket by prefix - There are many options for
-             * listing the objects in your bucket.  Keep in mind that buckets with
-             * many objects might truncate their results when listing their objects,
-             * so be sure to check if the returned object listing is truncated, and
-             * use the AmazonS3.listNextBatchOfObjects(...) operation to retrieve
-             * additional results.
-             */
-            System.out.println("Listing objects");
-            ObjectListing objectListing = s3.listObjects(new ListObjectsRequest()
-                    .withBucketName(bucketName)
-                    .withPrefix("My"));
-            for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
-                System.out.println(" - " + objectSummary.getKey() + "  " +
-                        "(size = " + objectSummary.getSize() + ")");
-            }
-            System.out.println();
-
-            /*
-             * Delete an object - Unless versioning has been turned on for your bucket,
-             * there is no way to undelete an object, so use caution when deleting objects.
-             */
-            System.out.println("Deleting an object\n");
-            s3.deleteObject(bucketName, key);
-
-            /*
-             * Delete a bucket - A bucket must be completely empty before it can be
-             * deleted, so remember to delete any objects from your buckets before
-             * you try to delete them.
-             */
-            System.out.println("Deleting bucket " + bucketName + "\n");
-            s3.deleteBucket(bucketName);
-        } catch (AmazonServiceException ase) {
-            System.out.println("Caught an AmazonServiceException, which means your request made it "
-                    + "to Amazon S3, but was rejected with an error response for some reason.");
-            System.out.println("Error Message:    " + ase.getMessage());
-            System.out.println("HTTP Status Code: " + ase.getStatusCode());
-            System.out.println("AWS Error Code:   " + ase.getErrorCode());
-            System.out.println("Error Type:       " + ase.getErrorType());
-            System.out.println("Request ID:       " + ase.getRequestId());
-        } catch (AmazonClientException ace) {
-            System.out.println("Caught an AmazonClientException, which means the client encountered "
-                    + "a serious internal problem while trying to communicate with S3, "
-                    + "such as not being able to access the network.");
-            System.out.println("Error Message: " + ace.getMessage());
+		RunInstancesResult runInstancesResult = ec2.runInstances(runInstancesRequest);
+		List<Instance> instances = runInstancesResult.getReservation().getInstances();
+		
+		System.out.println("RunInstance request has been sent");
+		
+		String instanceId = instances.get(0).getInstanceId();
+		
+		System.out.println(getInstanceInfo(instanceId));
+		
+		
+		boolean configured = false;
+		
+		do {
+			try {
+				
+				Thread.sleep(60 * 1000);
+				System.out.println(getInstanceInfo(instanceId));
+				InstanceInfo iInfo = getInstanceInfo(instanceId);
+				if (iInfo.getStatus().equals("running")) {
+						configureInstance(iInfo.getPublicIP());
+						configured = true;
+				}
+				
+			} catch (AmazonServiceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} while (!configured);
+		
+		return instanceId;
+	}
+	
+	public void terminateInstance(String instanceId)
+	{
+		ArrayList<String> instanceIds = new ArrayList<String>();
+		instanceIds.add(instanceId);
+		terminateInstances(instanceIds);
+	}
+	
+	public void terminateInstances(ArrayList<String> instanceIds)
+	{
+		try {
+        	// Terminate instances.
+        	TerminateInstancesRequest terminateRequest = new TerminateInstancesRequest(instanceIds);
+        	ec2.terminateInstances(terminateRequest);
+    	} catch (AmazonServiceException e) {
+    		// Write out any exceptions that may have occurred.
+           System.out.println("Error terminating instances");
+           System.out.println("Caught Exception: " + e.getMessage());
+           System.out.println("Reponse Status Code: " + e.getStatusCode());
+           System.out.println("Error Code: " + e.getErrorCode());
+           System.out.println("Request ID: " + e.getRequestId());
         }
-    }
+	}
+	
+	public InstanceInfo getInstanceInfo(String instanceId)
+	{
+		InstanceInfo iInfo = null;
+		
+		DescribeInstancesRequest describeInstancesRequest = new DescribeInstancesRequest().withInstanceIds(instanceId);
+		DescribeInstancesResult describeInstancesResult = ec2.describeInstances(describeInstancesRequest);
+		List<Reservation> list = describeInstancesResult.getReservations();
+		
+		for (Reservation res : list) {
+			List<Instance> instancelist = res.getInstances();
 
-    /**
-     * Creates a temporary file with text data to demonstrate uploading a file
-     * to Amazon S3
-     *
-     * @return A newly created temporary file with text data.
-     *
-     * @throws IOException
-     */
-    private static File createSampleFile() throws IOException {
-        File file = File.createTempFile("aws-java-sdk-", ".txt");
-        file.deleteOnExit();
+			for (Instance instance : instancelist) {
+				iInfo = InstanceInfo.ReadInfo(instance);
+			}
+		}
 
-        Writer writer = new OutputStreamWriter(new FileOutputStream(file));
-        writer.write("abcdefghijklmnopqrstuvwxyz\n");
-        writer.write("01234567890112345678901234\n");
-        writer.write("!@#$%^&*()-=[]{};':',.<>/?\n");
-        writer.write("01234567890112345678901234\n");
-        writer.write("abcdefghijklmnopqrstuvwxyz\n");
-        writer.close();
+		return iInfo;
+	}
+	
+	public ArrayList<String> getInstanceList()
+	{
+		ArrayList<String> instanceIds = new ArrayList<String>();
+		
+		DescribeInstancesRequest describeInstancesRequest = new DescribeInstancesRequest();
+		DescribeInstancesResult describeInstancesResult = ec2.describeInstances(describeInstancesRequest);
+		List<Reservation> list = describeInstancesResult.getReservations();
+		
+		for (Reservation res : list) {
+			List<Instance> instancelist = res.getInstances();
 
-        return file;
-    }
-
-    /**
-     * Displays the contents of the specified input stream as text.
-     *
-     * @param input
-     *            The input stream to display as text.
-     *
-     * @throws IOException
-     */
-    private static void displayTextInputStream(InputStream input) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        while (true) {
-            String line = reader.readLine();
-            if (line == null) break;
-
-            System.out.println("    " + line);
-        }
-        System.out.println();
-    }
+			for (Instance instance : instancelist) {
+				if(instance.getKeyName().equals("joseph_wing"))
+					instanceIds.add(instance.getInstanceId());
+			}
+		}
+		return instanceIds;
+	}
+	
+	public void configureInstance(String publicIP)
+	{
+		try {
+			final ProcessBuilder pb = new ProcessBuilder("/bin/sh", "initInstance", publicIP , "joseph_wing.pem");
+			pb.directory(new File("."));
+			pb.redirectErrorStream(true);
+			final Process p = pb.start();
+			//final int processStatus = p.waitFor();
+			
+//		    InputStream is = p.getInputStream();
+//		    InputStreamReader isr = new InputStreamReader(is);
+//		    BufferedReader br = new BufferedReader(isr);
+//		    String line;
+//		    while ((line = br.readLine()) != null) {
+//		      System.out.println(line);
+//		    }
+		    System.out.println("Program terminated!");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
