2c8bbf5471473e6ee4ad1e014954504ffaef9c16
Wing Lung Ngai <w.l.ngai@student.tudelft.nl>
2013-11-02 03:12:30 +0100

    fix naming

--- src/main/java/nl/tudelft/ec2interface/EC2Interface.java
+++ src/main/java/nl/tudelft/ec2interface/EC2Interface.java
@@ -12,7 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-package com.amazonaws.samples;
+package nl.tudelft.ec2interface;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -52,7 +52,7 @@ import com.amazonaws.services.s3.model.S3ObjectSummary;
  *                   sample.
  * http://aws.amazon.com/security-credentials
  */
-public class S3Sample {
+public class EC2Interface {
 
     public static void main(String[] args) throws IOException {
         /*
